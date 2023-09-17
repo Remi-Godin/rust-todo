9bb9d2b22df5ace505b75334f87edb55b682889a
regodin <regodin@pop-os.localdomain>
2023-09-17 01:54:20 -0300

    started experimenting towards a working product

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use console::Term;
+use std::time::SystemTime;
 
 fn main() {
-    let mut todo_list: Vec<TodoEntry> = Vec::with_capacity(10);
+    let mut todo_list: Vec<TodoEntry> = Vec::new();
     let term = Term::stdout();
     loop {
-        term.clone().clear_screen();
+        let _ = term.clone().clear_screen();
         print_entries(&todo_list, 0);
         todo_list.push(create_entry(term.clone()));
+        manage_entries(term.clone());
     }
 }
 
@@ -17,26 +19,42 @@ fn print_entries(entry_list: &Vec<TodoEntry>, count: i32) {
 }
 
 fn create_entry(term: Term) -> TodoEntry {
-    term.write_line("Enter task description...");
+    let _ = term.write_line("Enter task description...");
     let desc = term.read_line();
-    term.clear_last_lines(2);
+    let _ = term.clear_last_lines(2);
     let new_entry = TodoEntry {
         complete: false,
-        description: desc.unwrap_or_default()
+        description: desc.unwrap_or_default(),
+        timestamp: SystemTime::now(),
     };
-    return new_entry
+    return new_entry;
+}
+
+fn manage_entries(term: Term) {
+    loop {
+        let input = term.read_char().unwrap_or_default();
+        let _ = match input {
+            'h' => term.move_cursor_left(1),
+            'j' => term.move_cursor_down(1),
+            'k' => term.move_cursor_up(1),
+            'l' => term.move_cursor_right(1),
+            _ => break,
+        };
+    }
+}
+
+struct TodoEntry {
+    complete: bool,
+    description: String,
+    timestamp: SystemTime,
 }
 
 impl std::fmt::Display for TodoEntry {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut flag = "[ ]";
-        if self.complete == true{
+        if self.complete == true {
             flag = "[X]";
         }
-        write!(f, "{} {}", flag, self.description)
+        write!(f, "{:?} {} {}", self.timestamp, flag, self.description)
     }
-}
-struct TodoEntry {
-    complete: bool,
-    description: String
 }
