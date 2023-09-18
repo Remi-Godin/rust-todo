98c4121b3880ae8add7657894d8898514a3ef96e
regodin <regodin@pop-os.localdomain>
2023-09-17 23:23:03 -0300

    fixed a few issues with undefined inputs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use console::Term;
-use std::time::SystemTime;
 
 fn main() {
     let mut todo_list: Vec<TodoEntry> = Vec::new();
@@ -14,7 +13,10 @@ fn main() {
 fn main_menu(term: &Term, todo_list: &mut Vec<TodoEntry>) {
     let _ = term.write_line("(N)ew entry, (C)omplete entries");
     let _ = match term.read_key().unwrap() {
-        console::Key::Char('n') => todo_list.push(create_entry(term.clone()).unwrap()),
+        console::Key::Char('n') => match create_entry(term.clone()) {
+            Some(r) => todo_list.push(r),
+            None => print!("")
+        }
         console::Key::Char('c') => {complete_entries(todo_list, &select_entries(term.clone()));
             return}
         _ => println!("Unknown command")
@@ -30,6 +32,7 @@ fn print_entries(entry_list: &Vec<TodoEntry>) -> &Vec<TodoEntry>{
 }
 
 fn create_entry(term: Term) -> Option<TodoEntry> {
+    let _ = term.clear_last_lines(1);
     let _ = term.write_line("Enter task description, or press [enter] to exit...");
     let desc = match term.read_line() {
         Err(e) => panic!("An error has occured when reading user input: {}", e),
@@ -42,13 +45,12 @@ fn create_entry(term: Term) -> Option<TodoEntry> {
     let new_entry = TodoEntry {
         complete: false,
         description: desc,
-        _timestamp: SystemTime::now(),
-        _completed_timestamp: SystemTime::now()
     };
     return Some(new_entry);
 }
 
 fn select_entries(term: Term) -> Vec<usize>{
+    let _ = term.clear_last_lines(1);
     let _ = term.write_line("Enter the number of each tasks you want to act on, separated by spaces...");
     let args = match term.read_line() {
         Err(e) => panic!("An error as occured when reading user input: {}", e),
@@ -56,34 +58,25 @@ fn select_entries(term: Term) -> Vec<usize>{
     };
     let mut args_vec: Vec<usize> = Vec::new();
     for i in args.split(" ") {
-        args_vec.push(i.trim().parse::<usize>().unwrap_or_default());
+        if i.parse::<usize>().is_ok() {
+            args_vec.push(i.parse::<usize>().unwrap());
+        }
     }
     args_vec
 }
 
 fn complete_entries(todo_list: &mut Vec<TodoEntry>, entries: &Vec<usize>) {
     for i in entries.iter() {
-        todo_list.get_mut(*i).unwrap().complete();
+        if i < &todo_list.len(){
+            todo_list.get_mut(*i).unwrap().complete();
+        }
     }
     print_entries(&todo_list);
-}
-
-fn _manage_entries(term: Term) {
-    loop {
-        let input = term.read_char().unwrap_or_default();
-        let _ = match input {
-            'j' => term.move_cursor_down(1),
-            'k' => term.move_cursor_up(1),
-            _ => break,
-        };
-    }
 }
 
 struct TodoEntry {
     complete: bool,
     description: String,
-    _timestamp: SystemTime,
-    _completed_timestamp: SystemTime,
 }
 
 impl std::fmt::Display for TodoEntry {
